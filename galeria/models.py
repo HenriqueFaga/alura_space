80da2b7c27ee73e10b5f5ac16a2172c9df1de300
Henrique Lavieri <henrique.lavieri@softrh.com.br>
2024-07-26 16:35:03 -0300

    Aulas segunda parte (Persistencia de Dados).

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+    # Colunas:
+
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALÁXIA", "Galáxia"),
+        ("PLANETA", "Planeta")
+    ]
+
+    # CharField -> String;
+    # max_length maximo de char.
+    # null=False nao pode ser nulo.
+    # blank=False nao pode ser string vazia!
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    # TextField -> campo de texto
+    descriacao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=150, choices=OPCOES_CATEGORIA, default='')
+
+    def __str__(self):
+        # Boa pratica, devolve o nome de cada um dos itens.
+        return f"Fotografia [nome={self.nome}]"
--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    # request, template_name, context=None, content_type=None, status=None, using=None
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', context={'cards': fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
